2805bba713a878c9fdf6d088518236a7b7b7a29e
emilioSp <emilio.spatola@gmail.com>
2020-10-30 16:34:40 +0100

    Focus on search box

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,22 @@
 import './Search.css';
+import React from 'react';
 
-function Search(props) {
-  return (
-    <div className='search-box'>
-      <div><input type="text" value={props.filterText} onChange={props.onFilterTextChange}/></div>
-      <div><label><input type="checkbox" checked={props.filterOnlyInWareHouse} onChange={props.onFilterOnlyInWareHouseChange}/> Show only available products</label></div>
-    </div>
-  )
+export class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.searchInput = React.createRef();
+  }
+
+  componentDidMount() {
+    this.searchInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div className='search-box'>
+        <div><input type="text" value={this.props.filterText} onChange={this.props.onFilterTextChange} ref={this.searchInput} /></div>
+        <div><label><input type="checkbox" checked={this.props.filterOnlyInWareHouse} onChange={this.props.onFilterOnlyInWareHouseChange}/> Show only available products</label></div>
+      </div>
+    )
+  }
 }
-
-export { Search };
