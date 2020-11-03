80a23ef4c1fffbe0d7ce2508f2fa41f1f3856d02
emilioSp <emilio.spatola@gmail.com>
2020-11-03 17:58:07 +0100

    Adjust grid

--- src/App.js
+++ src/App.js
@@ -40,8 +40,6 @@ function App() {
         <Warehouse/>
       </WareHouseProvider>
       <Dummy/>
-    </div>
-    <div>
       <ResponsiveImg/>
     </div>
     </Fragment>
--- src/ResponsiveImg.js
+++ src/ResponsiveImg.js
@@ -1,6 +1,8 @@
 export const ResponsiveImg = () =>
-  <picture>
+ <div>
+   <picture>
     <source media="(min-width:1600px)" srcSet="welcome01-1600.jpg"/>
     <source media="(min-width:800px)" srcSet="welcome01-800.jpg"/>
-    <img src="welcome01-400.jpg" alt="Welcome" style={{width: "auto"}} />
+    <img src="welcome01-400.jpg" alt="Welcome" />
   </picture>
+</div>
