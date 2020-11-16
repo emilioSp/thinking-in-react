5446e60089f00c3ee85f11ea4a9ca27b5ce0204a
emilioSp <emilio.spatola@gmail.com>
2020-11-16 15:06:13 +0100

    useMemo test

--- src/Dummy.js
+++ src/Dummy.js
@@ -1,5 +1,15 @@
+import { useMemo } from 'react';
+
+const DummyInternal = props => {
+  console.log('Dummy Internal Memoized');
+  return <div>I'm the internal div {props.test}</div>;
+}
+
 export const Dummy = () => {
   console.log('Dummy');
-  return <div>I'm a dummy component out of context!</div>
+  const test = 'test';
+  return <div>I'm a dummy component out of context!
+    {useMemo(() => <DummyInternal test={test}></DummyInternal>, [test])}
+  </div>
 }
 
