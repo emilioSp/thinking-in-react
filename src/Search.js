845ffea24c9f91e395da3aa1f6e14447e1e25a2e
emilioSp <emilio.spatola@gmail.com>
2020-11-16 13:36:17 +0100

    useRef

--- src/Search.js
+++ src/Search.js
@@ -1,33 +1,29 @@
 import './Search.css';
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends React.Component {
-  static propTypes = {
-    placeHolder: PropTypes.string,
-    filterText: PropTypes.string.isRequired,
-    filterOnlyInWareHouse: PropTypes.bool.isRequired,
-    onFilterTextChange: PropTypes.func,
-    onFilterOnlyInWareHouseChange: PropTypes.func,
-  }
-  static defaultProps = {
-    placeHolder: 'Search in warehouse'
-  }
-  constructor(props) {
-    super(props);
-    this.searchInput = React.createRef();
-  }
+export const Search = props => {
+  const searchInput = useRef(null);
+  useEffect(() => {
+    searchInput.current.focus();
+  }, [])
 
-  componentDidMount() {
-    this.searchInput.current.focus();
-  }
-
-  render() {
-    return (
+  return (
       <div className='search-box'>
-        <div><input type="text" value={this.props.filterText} onChange={this.props.onFilterTextChange} ref={this.searchInput} placeholder={this.props.placeHolder} /></div>
-        <div><label><input type="checkbox" checked={this.props.filterOnlyInWareHouse} onChange={this.props.onFilterOnlyInWareHouseChange}/> Show only available products</label></div>
+        <div><input type="text" value={props.filterText} onChange={props.onFilterTextChange} ref={searchInput} placeholder={props.placeHolder} /></div>
+        <div><label><input type="checkbox" checked={props.filterOnlyInWareHouse} onChange={props.onFilterOnlyInWareHouseChange}/> Show only available products</label></div>
       </div>
     )
-  }
+}
+
+Search.propTypes = {
+  placeHolder: PropTypes.string,
+  filterText: PropTypes.string.isRequired,
+  filterOnlyInWareHouse: PropTypes.bool.isRequired,
+  onFilterTextChange: PropTypes.func,
+  onFilterOnlyInWareHouseChange: PropTypes.func,
+}
+
+Search.defaultProps = {
+  placeHolder: 'Search in warehouse'
 }
