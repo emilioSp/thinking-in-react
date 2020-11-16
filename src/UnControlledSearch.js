5db3aa693b23acd9a09ba7e264de1f6273bc208c
emilioSp <emilio.spatola@gmail.com>
2020-11-16 14:42:48 +0100

    useRef

--- src/UnControlledSearch.js
+++ src/UnControlledSearch.js
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-export class UnControlledSearch extends React.Component {
-  input;
-  constructor(props) {
-    super(props);
-    this.input = React.createRef();
-    this.handleOnChangeInput = this.handleOnChangeInput.bind(this);
+export const UnControlledSearch = props => {
+  const input = useRef(null);
+
+  function handleOnChangeInput() {
+    console.log(input.current.value);
   }
 
-  handleOnChangeInput() {
-    console.log(this.input.value);
-  }
-
-  render() {
-    return <div>
-      <label>UnController input <input type="text" ref={el => this.input = el} onChange={this.handleOnChangeInput}/></label>
-    </div>
-  }
+  return <div>
+    <label>UnController input <input type="text" ref={input} onChange={handleOnChangeInput}/></label>
+  </div>
 }
